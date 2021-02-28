49316fc2168f3344108847f89300ebbcfa8d7ef6
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-02-28 10:26:33 +0100

    added count down fuction

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const square = document.querySelectorAll('.square');
 const mole = document.querySelectorAll('.mole');
 const timeLeft = document.querySelector('#time-left');
-
 let score = document.querySelector('#score');
 
 let result = 0;
+let currentTime = timeLeft.textContent;
 
 function randomSquare() {
 	square.forEach((className) => {
 		className.classList.remove('mole');
 	});
-	let randomPosition = square[Math.floor(Math.random() + 9)];
+	let randomPosition = square[Math.floor(Math.random() * 9)];
 	randomPosition.classList.add('mole');
+
+	//assign the id of the randomPosition to hitPosition for us to use later
 	hitPosition = randomPosition.id;
 }
 
@@ -20,10 +22,26 @@ square.forEach((id) => {
 		if (id.id === hitPosition) {
 			result = result + 1;
 			score.textContent = result;
+			hitPosition = null;
 		}
 	});
 });
+
 function moveMole() {
 	let timerId = null;
-	timerId = setInterval(randomSquare, 1000);
+	timerId = setInterval(randomSquare, 500);
 }
+
+moveMole();
+
+function countDown() {
+	currentTime--;
+	timeLeft.textContent = currentTime;
+
+	if (currentTime === 0) {
+		clearInterval(timerId);
+		alert('GAME OVER! Your final score is' + result);
+	}
+}
+
+let timerId = setInterval(countDown, 1000);
